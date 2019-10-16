8a1826c5d4208668edf8ef774e3f38e402eab04b
Max Willian <borges.mwo@hotmail.com>
2019-10-16 13:10:30 -0400

    Adicionando exercícios

---
+++ Mundo1/Exercicios/ExerciciosMundo1.py
@@ -0,0 +1,7 @@
+#  DESAFIO 001
+#  print('Olá, mundo!')
+
+#  DESAFIO 002
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer {}!'.format(nome))  # Imprimindo com o método format da string
+print(f'É um prazer te conhecer {nome}!')  # Imprimindo com o f-strings
