b77cce15f32d0ba4ba392d869a24785ca3baebad
Max Willian <borges.mwo@hotmail.com>
2019-10-16 12:57:33 -0400

    adicionando script com desafios

---
+++ Mundo1/Exercicios/ScriptIdle1.py
@@ -0,0 +1,19 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite seu peso: ')
+print(nome, idade, peso)
+
+print('===== Desafio 01 =====')
+nome1 = input('Qual é o seu nome? ')
+print(f'Olá {nome1}! Prazer em te conhecer!')
+
+print('===== Desafio 02 =====')
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+print(f'Você nasceu no dia {dia} de {mes} de {ano}. Certo?')
+
+print('===== Desafio 03 =====')
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Primeiro número: '))
+print(f'A soma é {n1+n2}')
